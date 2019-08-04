2c1d19da0a832bfe66c440d21c680d0d5b51e332
klee2010 <klee2010@gmail.com>
2019-08-03 17:23:00 -0700

    initialize repo

---
+++ maximum-indepenent-set.js
@@ -0,0 +1,47 @@
+/*
+
+    Given a weighted graph path the functions below will find the maximum independent set
+
+*/
+
+const PATH = [1, 9, 3, 2, 8];
+
+// Recursively find max 
+const FindMaxISHelper = (P) => {
+    if (P.length === 0) {
+        return 0;
+    }
+    if (P.length === 1) {
+        return P[0];
+    }
+    const index = P.length;
+    return Math.max(
+        FindMaxISHelper(P.slice(0, index-1)),
+        FindMaxISHelper(P.slice(0, index-2)) + P[index - 1]
+    );
+}
+
+// Dynamic programming solution
+const FindMaxIS = (P) => {
+    let SOLN = [];
+
+    if (P.length === 0) {
+        SOLN[0] = 0;
+    }
+
+    if (P.length === 1) {
+        SOLN[1] = P[0];
+    }
+
+    if (P.length > 2) {
+        for (let k=2; k < P.length + 1; k++) {
+            SOLN[k] = FindMaxISHelper(P.slice(0, k));
+        }
+    }
+
+    return SOLN[P.length];
+}
+
+module.exports = {
+    FindMaxIS,
+}
---
+++ test/maximum-indepenent-set-test.js
@@ -0,0 +1,14 @@
+let assert = require('assert');
+let { FindMaxIS } = require('../maximum-indepenent-set');
+
+describe('FindMaxIS', function() {
+    it('should return 0 if passed an empty array', function() {
+      assert.equal(FindMaxIS([]), 0);
+    });
+    it('should return the weight of the first element if passed an array of 1 element', function() {
+      assert.equal(FindMaxIS([10]), 10);
+    });
+    it('should return the maximum weight of an indepdent set possible in the path', function() {
+      assert.equal(FindMaxIS([1, 9, 3, 2, 8]), 17);
+    });
+});
